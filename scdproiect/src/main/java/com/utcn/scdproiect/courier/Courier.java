dc8f758fbb89e4f81c5efc9d39620300c297f020
ClaudiuA1 <161977282+ClaudiuA1@users.noreply.github.com>
2024-11-09 00:19:20 +0200

    Merge si curierul acuma

--- scdproiect/src/main/java/com/utcn/scdproiect/courier/Courier.java
+++ scdproiect/src/main/java/com/utcn/scdproiect/courier/Courier.java
@@ -4,16 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Courier {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
-    private Courier manager_id;
+    private Integer manager_id;
 
 }
